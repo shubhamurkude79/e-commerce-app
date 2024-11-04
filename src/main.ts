024349ae7be6ef8059da84baa9b69b47682db66e
shubhamurkude79 <shubhamurkude79@gmail.com>
2024-11-04 23:04:08 +0530

    setup Apollo client in Angular to connect with graphQL server

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,22 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { ApolloClient, InMemoryCache } from '@apollo/client/core';
+import { HttpLink } from '@apollo/client/link/http';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import { AppComponent } from './app/app.component';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { ApolloProvider } from '@apollo/client';
+
+const graphqlUri = 'https://your-graphql-endpoint.com/graphql'; // replace with your GraphQL server endpoint
+
+const apolloClient = new ApolloClient({
+  link: new HttpLink({ uri: graphqlUri }),
+  cache: new InMemoryCache(),
+});
+
+  bootstrapApplication(AppComponent, {
+    providers: [
+      provideHttpClient(withInterceptors([])),
+      { provide: ApolloProvider, useValue: apolloClient },
+    ],
+  }).catch((err) => console.error(err));
+  
