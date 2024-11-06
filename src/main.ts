ab2c3c6b820825544cefa9ed37ff50ad3bde93d4
shubhamurkude79 <shubhamurkude79@gmail.com>
2024-11-06 23:24:18 +0530

    working graphQL server uri added

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { ApolloClient, InMemoryCache } from '@apollo/client/core';
 import { HttpLink } from '@apollo/client/link/http';
 
 import { AppComponent } from './app/app.component';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApolloProvider } from '@apollo/client';
 
-const graphqlUri = 'https://your-graphql-endpoint.com/graphql'; // replace with your GraphQL server endpoint
+const graphqlUri = 'http://localhost:4000/graphql'; // replace with your GraphQL server endpoint
 
 const apolloClient = new ApolloClient({
   link: new HttpLink({ uri: graphqlUri }),
