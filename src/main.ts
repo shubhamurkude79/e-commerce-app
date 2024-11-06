cf5cad252e016c07808e6e78cd868b7b95d4cc89
shubhamurkude79 <shubhamurkude79@gmail.com>
2024-11-06 23:46:07 +0530

    apollo-angular installed and made changes based on it in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,25 @@
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { ApolloClient, InMemoryCache } from '@apollo/client/core';
+import { InMemoryCache } from '@apollo/client/core';
 import { HttpLink } from '@apollo/client/link/http';
-
+import {  APOLLO_OPTIONS } from 'apollo-angular';
 import { AppComponent } from './app/app.component';
-import { ApolloProvider } from '@apollo/client';
+
 
 const graphqlUri = 'http://localhost:4000/graphql'; // replace with your GraphQL server endpoint
 
-const apolloClient = new ApolloClient({
+const apolloClientOptions = {
   link: new HttpLink({ uri: graphqlUri }),
   cache: new InMemoryCache(),
-});
+};
 
-  bootstrapApplication(AppComponent, {
-    providers: [
-      provideHttpClient(withInterceptors([])),
-      { provide: ApolloProvider, useValue: apolloClient },
-    ],
-  }).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(withInterceptors([])),
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: () => apolloClientOptions,
+    },
+  ],
+}).catch((err) => console.error(err));
   
