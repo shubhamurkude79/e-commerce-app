1294411dcfde9cf8c49a69150654c50c373dd3ff
shubhamurkude79 <shubhamurkude79@gmail.com>
2024-11-10 23:16:52 +0530

    navbar and productlists components added into app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProductListsComponent } from './components/product-list/product-list.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProductListComponent],
+  imports: [CommonModule, RouterOutlet, ProductListComponent, NavbarComponent, ProductListsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
