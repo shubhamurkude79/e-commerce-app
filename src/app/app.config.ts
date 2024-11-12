ae15d6e6817714d5892312f753485385510fcbaa
shubhamurkude79 <shubhamurkude79@gmail.com>
2024-11-12 23:27:53 +0530

    routing configuration set for two components

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,13 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, Route } from '@angular/router';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 
-import { routes } from './app.routes';
+const routes: Route[] = [
+  { path: '', component: ProductListComponent },
+  { path: 'product/:id', component: ProductDetailComponent },
+  // Add more routes as needed
+];
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
