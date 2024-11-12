fedee62c65b74b55c77b74416e7ba9e7eb4c2b90
shubhamurkude79 <shubhamurkude79@gmail.com>
2024-11-12 23:33:48 +0530

    product details based on each id functionality fetched from graphQL server

--- src/app/components/product-detail/product-detail.component.ts
+++ src/app/components/product-detail/product-detail.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Apollo, gql } from 'apollo-angular';
+import { ProductInterface } from '../../models/product.model';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-product-detail',
@@ -8,5 +12,36 @@ import { Component } from '@angular/core';
   styleUrl: './product-detail.component.css'
 })
 export class ProductDetailComponent {
+  private route = inject(ActivatedRoute);
+  private apollo = inject(Apollo);
+  product: ProductInterface | null = null;
+
+  ngOnInit(): void {
+    const productId = this.route.snapshot.paramMap.get('id');
+
+    if (productId) {
+      const GET_PRODUCT_BY_ID = gql`
+        query GetProduct($id: ID!) {
+          product(id: $id) {
+            id
+            name
+            description
+            price
+            imageUrl
+          }
+        }
+      `;
+
+      this.apollo
+        .watchQuery<any>({
+          query: GET_PRODUCT_BY_ID,
+          variables: { id: productId },
+        })
+        .valueChanges.pipe(map((result) => result.data.product))
+        .subscribe((product) => {
+          this.product = product;
+        });
+    }
+  }
 
 }
