96c9a47a7195d20d7c5b328c832a092359d103ed
shubhamurkude79 <shubhamurkude79@gmail.com>
2024-11-23 23:49:52 +0530

    cart route and wildcard routings added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,13 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, Route } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { CartComponent } from './components/cart/cart.component';
 
 const routes: Route[] = [
-  { path: '', component: ProductListComponent },
+  { path: 'products', component: ProductListComponent },
+  { path: 'cart', component: CartComponent},
   { path: 'product/:id', component: ProductDetailComponent },
+  { path: '**', redirectTo:'products', pathMatch: 'full'}
   // Add more routes as needed
 ];
 
