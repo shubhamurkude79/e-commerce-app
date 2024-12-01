b80afd5c8102b26d266649b52c83526f26e0e9fc
shubhamurkude79 <shubhamurkude79@gmail.com>
2024-12-02 01:28:40 +0530

    Cart selectors created to query specific parts of the state

---
+++ src/app/store/cart/cart.selectors.ts
@@ -0,0 +1,14 @@
+import { createFeatureSelector, createSelector } from "@ngrx/store";
+import { CartState } from "./cart.state";
+
+export const selectCartState = createFeatureSelector<CartState>('cart');
+
+export const selectCartItems = createSelector(
+    selectCartState,
+    state => state.items
+);
+
+export const selectCartTotal = createSelector(
+    selectCartItems,
+    items => items.reduce((total, item) => total + item.price * item.quantity, 0)
+);
