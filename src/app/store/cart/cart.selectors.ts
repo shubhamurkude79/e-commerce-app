ad5ac43d354f90e87e9723611379636af8005628
shubhamurkude79 <shubhamurkude79@gmail.com>
2024-12-16 22:34:27 +0530

    ECA-1: For Cart Badge, updated cart.selector with selectCartItemCount

--- src/app/store/cart/cart.selectors.ts
+++ src/app/store/cart/cart.selectors.ts
@@ -12,3 +12,8 @@ export const selectCartTotal = createSelector(
     selectCartItems,
     items => items.reduce((total, item) => total + item.price * item.quantity, 0)
 );
+
+export const selectCartItemCount = createSelector(
+    selectCartItems,
+    items => items.reduce((count, item) => count + item.quantity, 0)
+);
