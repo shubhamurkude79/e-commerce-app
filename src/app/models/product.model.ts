cffd6cdac870db6583ada9c9d52658e3c8baba17
shubhamurkude79 <shubhamurkude79@gmail.com>
2024-12-18 01:50:32 +0530

    ECA-2: Product interface updated with category and brand

--- src/app/models/product.model.ts
+++ src/app/models/product.model.ts
@@ -2,6 +2,8 @@ export interface ProductInterface {
     id: string;
     name: string;
     description: string;
+    category: string;
+    brand: string;
     price: number;
     imageUrl: string;
 }
