fceb7c88b404817beaa7be8fcf4522c1c5a41782
shubhamurkude79 <shubhamurkude79@gmail.com>
2024-12-27 03:44:01 +0530

    ECA-5: banner images and autoplay slider added

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { AfterViewInit, Component, OnInit, signal } from '@angular/core';
 import { ProductInterface } from '../../models/product.model';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
@@ -12,9 +12,16 @@ import { ProductService } from '../../services/product.service';
   styleUrl: './product-list.component.css'
 })
 
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, AfterViewInit {
   // Initialize the signal to store products
   products = signal<ProductInterface[]>([]);
+  sliderImages = signal<string[]>([
+    '/images/banner/banner1.jpeg',
+    '/images/banner/banner2.png',
+    '/images/banner/banner3.png',
+    '/images/banner/banner4.png',
+  ]);
+  myIndex: number = 0;
 
   constructor(private productService: ProductService) {}
 
@@ -23,5 +30,24 @@ export class ProductListComponent implements OnInit {
     this.productService.getProducts().subscribe((products) => {
       this.products.set(products)
     })
+  }
+  ngAfterViewInit(): void {
+    this.carousel();
+  }
+
+  carousel(): void {
+    const slides = document.getElementsByClassName("mySlides") as HTMLCollectionOf<HTMLElement>;
+    if (!slides.length) {
+      return; // Safeguard in case slides are not available
+    }
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    this.myIndex++;
+    if (this.myIndex > slides.length) {
+      this.myIndex = 1;
+    }
+    slides[this.myIndex - 1].style.display = "block";
+    setTimeout(() => this.carousel(), 3500); // Change image every 2 seconds
   }
 }
