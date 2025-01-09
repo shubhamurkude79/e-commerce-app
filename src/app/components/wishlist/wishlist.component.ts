8d1fa318e11574f24c284ece115e20fae62a438c
shubhamurkude79 <shubhamurkude79@gmail.com>
2025-01-10 01:33:08 +0530

    ECA-6: wishlist component made functional with its basics ui

--- src/app/components/wishlist/wishlist.component.ts
+++ src/app/components/wishlist/wishlist.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Store } from '@ngrx/store';
+import { selectWishlistItems } from '../../store/wishlist/wishlist.selectors';
+import { removeFromWishlist } from '../../store/wishlist/wishlist.actions';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-wishlist',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './wishlist.component.html',
   styleUrl: './wishlist.component.css'
 })
-export class WishlistComponent {
+export class WishlistComponent implements OnInit{
+  wishlist$!: Observable<any>;
+
+  constructor(private store: Store){}
+
+  ngOnInit(): void {
+    this.wishlist$ = this.store.select(selectWishlistItems);
+  }
+
+  removeItem(id: string): void {
+    this.store.dispatch(removeFromWishlist({ id }));
+  }
 
 }
